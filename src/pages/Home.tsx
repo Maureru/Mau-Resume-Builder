5b0f15f3317249c6dec03bd71ca7e691150ef202
xMauu <102646969+xMauu@users.noreply.github.com>
2022-12-28 13:18:40 +0800

    fix image load

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -50,7 +50,7 @@ function Home() {
         </AnimatePresence>
         <div id='header'  className='mb-4 mt-8 lg:mt-0 min-h-[calc(100vh-4rem)] flex flex-col lg:grid grid-cols-2 w-full'>
             <div className='overflow-hidden order-2 lg:order-first my-8 lg:my-0 select-none flex justify-center'>
-                <img src='../src/assets/avatar.png' alt='' className='h-full'/>
+                <img src='/assets/avatar.png' alt='' className='h-full'/>
             </div>
             <div className='flex flex-col justify-center'>
                 <div>
